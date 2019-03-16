c7fd803c6585ae1ea1d7f5031a6a383f97f7fd2c
cdclaw <langluo9843@gmail.com>
2019-03-15 23:42:14 -0700

    revert

--- app/components/App.js
+++ app/components/App.js
@@ -1,41 +1,66 @@
-// // var React = require('react');
-// import React from 'react';
-// import {BrowserRouter as Router, Link} from 'react-router-dom';
-// import Route from 'react-router-dom/Route';
+import React, {Component} from 'react';
+var ReactDOM = require('react-dom');
+import Login from './Login';
+import MainPage from './MainPage';
+import {BrowserRouter as Router, Link} from 'react-router-dom';
+import Route from 'react-router-dom/Route';
+import fire from './Firebase/firebase';
 
+var Team = require('./data/team');
+var Team1 = new Team(1,
+  "teamname",
+  "appname",
+  "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore eius quo quis quibusdam explicabo praesentium ut aliquam libero at ex! Alias voluptates optio obcaecati molestias placeat necessitatibus, cum tenetur quidem.");
 
-// // var Login = require('./Login');
-// import Login from './Login';
+class App extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      user: {},
+      teams: []
+    }
+  }
+  // Retrieve data from firestore
+  getTeamData(){
+    console.log("get team data...");
+    teamRef.get().then(function(querySnapshot){
+      listofTeams.pop();
+      querySnapshot.forEach(function(doc){
+        var temp = new Team("2", doc.data().teamName, doc.data().appName, doc.data().appDescription);
+        listofTeams.push(temp);
+        console.log(doc.id, " => ", doc.data());
+      })
+    })
+    
+  }
+  componentDidMount(){
+    this.authListener();
+    this.getTeamData();
+  }
 
+  authListener() {
+    fire.auth().onAuthStateChanged ((user)=> {
+      // console.log(user);
+      if (user) {
+        this.setState({user});
+        // localStorage.setItem('user', user.uid);
+      } else{
+        this.setState({user: null});
+        // localStorage.removeItem('user');
+      }
+    })
+  }
+  render(){
+    
+    return(
+      <div>
+      {this.state.user ? (<MainPage teams={listofTeams}></MainPage>) : (<Login></Login>)}
+      {/* {this.state.user ? (<MainPage teams={this.state.teams}></MainPage>) : (<Login></Login>)} */}
 
-// class App extends React.Component{
-//   // constructor(props){
-//   //   super(props);
-//   //   this.state ={};
-//   // }
-//   render(){
-//     return(
-//       <Router>
-//         <div>
-//           <ul>
-//             <li><Link to="/">Home</Link></li>
-//             <li><Link to="/about">About</Link></li>
+      </div>
+        
 
-//           </ul>
-          
-
-//           <Route path="/"exact={true} component={Login} />
-//           <Route path="/about" exact strict render={
-//             ()=> {
-//               return (<h1> welcome about</h1>)
-//             }
-//           } />
-//           <Route path="/user/:username" exact strict component={User}
-//           />
-//         </div>
-//       </Router>
-//     )
-//   }
-// }
-// // module.exports = App;
-// export default App;
+    );
+  }
+}
+export default App;
--- app/index.js
+++ app/index.js
@@ -1,45 +1,100 @@
 import React, {Component} from 'react';
 var ReactDOM = require('react-dom');
 import Login from './components/Login';
+import App from './components/App';
 import MainPage from './components/MainPage';
+// import fire from './components/Firebase/firebase';
 
 import {BrowserRouter as Router, Link} from 'react-router-dom';
 import Route from 'react-router-dom/Route';
 var Team = require('./data/team');
-var Team1 = new Team(1,
-                    "teamname",
-                    "appname",
-                    "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore eius quo quis quibusdam explicabo praesentium ut aliquam libero at ex! Alias voluptates optio obcaecati molestias placeat necessitatibus, cum tenetur quidem.");
 
-class App extends Component{
-  // constructor(props){
-  //     super(props);
-  //     this.state ={};
-  // }
-  render(){
+var Team1 = new Team("1",
+                    "Gogo",
+                    "uber",
+                    "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore eius quo quis quibusdam explicabo praesentium ut aliquam libero at ex! Alias voluptates optio obcaecati molestias placeat necessitatibus, cum tenetur quidem.",
+                    0);
+
+
+var listofTeams = [Team1];
+
+var db = fire.firestore();
+var eventRef = db.collection("events").doc("event19");
+var teamRef = eventRef.collection("teams");
+var tempTeam  = [];
+
+// class App extends Component{
+  
+//   constructor(props){
+//     super(props);
+//     this.state = {
+//       user: {},
+//       teams: []
+//     }
+//   }
+
+  // Retrieve data from firestore
+  // getTeamData(){
+  //   console.log("get team data...");
+  //   teamRef.get().then(function(querySnapshot){
+  //     listofTeams.pop();
+  //     querySnapshot.forEach(function(doc){
+  //       var temp = new Team("2", doc.data().teamName, doc.data().appName, doc.data().appDescription);
+  //       listofTeams.push(temp);
+  //       console.log(doc.id, " => ", doc.data());
+  //     })
+  //   })
     
-      return(
-        <Router>
-          <div>
-            {/* <ul>
-              <li><Link to="/">Home</Link></li>
-              <li><Link to="/about">About</Link></li>
+  // }
+  // componentDidMount(){
+  //   this.authListener();
+  //   this.getTeamData();
+  // }
 
-            </ul> */}
-            
+  // authListener() {
+  //   fire.auth().onAuthStateChanged ((user)=> {
+  //     // console.log(user);
+  //     if (user) {
+  //       this.setState({user});
+  //       // localStorage.setItem('user', user.uid);
+  //     } else{
+  //       this.setState({user: null});
+  //       // localStorage.removeItem('user');
+  //     }
+  //   })
+  // }
+//   render(){
+      
+//       return(
+//         <div>
+//         {this.state.user ? (<MainPage teams={listofTeams}></MainPage>) : (<Login></Login>)}
+//         {/* {this.state.user ? (<MainPage teams={this.state.teams}></MainPage>) : (<Login></Login>)} */}
 
-            {/* <Route path="/" exact={true} component={Login} /> */}
-            <Route path="/" exact strict component={() => <MainPage team={Team1}></MainPage>}/>
-          </div>
-        </Router>
+//         </div>
           
 
-      );
-  }
-}
+//       );
+//   }
+// }
 
 ReactDOM.render(
   <App 
   />, 
   document.getElementById('app')
 )
+
+
+// <Router>
+//           <div>
+//             {/* <ul>
+//               <li><Link to="/">Home</Link></li>
+//               <li><Link to="/about">About</Link></li>
+
+//             </ul> */}
+            
+//             {/* <Route path="/" exact={true} component={Login} /> */}
+
+//             {/* <Route path="/" exact strict component={() => <MainPage teams={listofTeams}></MainPage>}/> */}
+
+//           </div>
+//         </Router>
