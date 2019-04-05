fe60b3e8e0bfeb7625c0a42b6bdc030c9d9940ea
cdclaw <langluo9843@gmail.com>
2019-04-05 03:11:09 -0700

    login responsive layout

--- app/components/LoginPage.js
+++ app/components/LoginPage.js
@@ -38,27 +38,27 @@ class Login extends React.Component{
       
       <Container fluid={true}>
         <Row className="login-row">
-          <Col className="login-col"sm={3}></Col>
-          <Col className="login-col"sm={6}><img className="login-logo1" src={require('../assets/dfs_programlogo_appjam_stacked.png')} alt="Logo"></img></Col>
-          <Col className="login-col"sm={3}></Col>
+          <Col className="login-col"sm={2} lg={3} xl={4}></Col>
+          <Col className="login-col"xs={12} sm={8} lg={6} xl={4}><img className="login-logo1" src={require('../assets/dfs_programlogo_appjam_stacked.png')} alt="Logo"></img></Col>
+          <Col className="login-col"sm={2} lg={3} xl={4}></Col>
         </Row>
         <Row>
-          <Col className="login-col"sm={3}></Col>
-          <Col className="login-col"sm={6}><p className="sys-name">Judge</p></Col>
-          <Col className="login-col"sm={3}></Col>
+          <Col className="login-col"sm={2} lg={3} xl={4}></Col>
+          <Col className="login-col"xs={12} sm={8} lg={6} xl={4}><p className="sys-name">Judge</p></Col>
+          <Col className="login-col"sm={2} lg={3} xl={4}></Col>
 
         </Row>
         <Row className="login-row">
-          <Col className="login-col"sm={3}></Col>
-          <Col className="login-col login-box"sm={6}>
+          <Col className="login-col"sm={2} lg={3} xl={4}></Col>
+          <Col className="login-col login-box"xs={12} sm={8} lg={6} xl={4}>
             <Row className="login-row">
-              <Col className="login-col" sm={1}></Col>
-              <Col className="login-col" sm={10}><div className="signin-text">SIGN IN TO YOUR ACCOUNT</div></Col>
-              <Col className="login-col" sm={1}></Col>
+              <Col className="login-col" lg={1}></Col>
+              <Col className="login-col" lg={10}><div className="signin-text">SIGN IN TO YOUR ACCOUNT</div></Col>
+              <Col className="login-col" lg={1}></Col>
             </Row>
             <Row>
-              <Col className="login-col" sm={2}></Col>
-              <Col className="login-col" sm={8}>
+              <Col className="login-col" lg={2}></Col>
+              <Col className="login-col" lg={8}>
                 <form method="post" onSubmit={this.submitLogin.bind(this)}>
                   <input className="login-input-field" type="text" name="email" placeholder="Email" onChange={this.onEmailChange.bind(this)}/>
                   <input className="login-input-field" type="password" name="password" placeholder="Password" onChange={this.onPasswordChange.bind(this)}/>
@@ -66,16 +66,16 @@ class Login extends React.Component{
                 </form>
                 
               </Col>
-              <Col className="login-col" sm={2}></Col>
+              <Col className="login-col" lg={2}></Col>
             </Row>
           </Col>
-          <Col className="login-col"sm={3}></Col>
+          <Col className="login-col"sm={2} lg={3} xl={4}></Col>
         </Row>
 
         <Row className="login-row">
-          <Col className="login-col"sm={3}></Col>
-          <Col className="login-col"sm={6}><img className="login-logo2" src={require('../assets/dfs_logo_fullcolor_tagline.png')} alt="Logo"></img></Col>
-          <Col className="login-col"sm={3}></Col>
+          <Col className="login-col"lg={3} xl={4}></Col>
+          <Col className="login-col"xs={12} lg={6} xl={4}><img className="login-logo2" src={require('../assets/dfs_logo_fullcolor_tagline.png')} alt="Logo"></img></Col>
+          <Col className="login-col"lg={3} xl={4}></Col>
         </Row>
       </Container>
       
